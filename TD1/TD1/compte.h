ccf368f86b8c10ae9d4a4b6895692d46b36c5e20
Nyx <83186817+orph3nix@users.noreply.github.com>
2022-03-10 15:46:45 +0100

    Update compte.h

--- TD1/TD1/compte.h
+++ TD1/TD1/compte.h
@@ -1,7 +1,25 @@
 #pragma once
+#include <string>
+using namespace std;
 class compte
 {
-	double solde;
-	Client proprietaire;
-};
+    double solde;
+    client proprietaire;
 
+    public:compte(double solde, client proprietaire)
+    {
+        this->solde = solde;
+        this->proprietaire = proprietaire;
+    }
+    
+    void Afficher()
+    {
+        cout << "\n solde : " << solde;
+        proprietaire.Afficher();
+    }
+
+    void retrait(double Somme) 
+    {
+        this->solde == solde - Somme;
+    }
+};
